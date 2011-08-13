e036a05c56a643bc9f63bd3b66802ab7adf14235
Cornelius Schumacher <schumacher@kde.org>
2011-08-13 13:34:35 +0200

    Clean up main function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
+/*
+    This file is part of KDE.
+
+    Copyright (C) 2009-2011 Cornelius Schumacher <schumacher@kde.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+    USA.
+*/
+
 #include "mainwindow.h"
 
 #include <kapplication.h>
@@ -6,49 +27,28 @@
 #include <KDE/KLocale>
 
 static const char description[] =
-    I18N_NOOP("A KDE 4 Application");
+  I18N_NOOP("The humane address book for the cloud");
 
 static const char version[] = "0.1";
 
 int main(int argc, char **argv)
 {
-    KAboutData about("polka", 0, ki18n("Polka"), version, ki18n(description),
-                     KAboutData::License_GPL, ki18n("(C) 2007 Cornelius Schumacher"), KLocalizedString(), 0, "schumacher@kde.org");
-    about.addAuthor( ki18n("Cornelius Schumacher"), KLocalizedString(), "schumacher@kde.org" );
-    KCmdLineArgs::init(argc, argv, &about);
+  KAboutData about( "polka", 0, ki18n("Polka"), version, ki18n(description),
+    KAboutData::License_GPL, ki18n("(C) 2011 Cornelius Schumacher"),
+    KLocalizedString(), 0, "schumacher@kde.org");
+  about.addAuthor( ki18n("Cornelius Schumacher"), KLocalizedString(),
+    "schumacher@kde.org" );
+  KCmdLineArgs::init( argc, argv, &about );
 
-    KCmdLineOptions options;
-    options.add("+[URL]", ki18n( "Document to open" ));
-    KCmdLineArgs::addCmdLineOptions(options);
-    KApplication app;
+  KApplication app;
 
-    MainWindow *widget = new MainWindow;
+  MainWindow *widget = new MainWindow;
 
-    // see if we are starting with session management
-    if (app.isSessionRestored())
-    {
-        RESTORE(MainWindow);
-    }
-    else
-    {
-        // no session.. just start up normally
-        KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
-        if (args->count() == 0)
-        {
-            //MainWindow *widget = new MainWindow;
-            widget->show();
-        }
-        else
-        {
-            int i = 0;
-            for (; i < args->count(); i++)
-            {
-                //MainWindow *widget = new MainWindow;
-                widget->show();
-            }
-        }
-        args->clear();
-    }
+  if ( app.isSessionRestored() ) {
+    RESTORE(MainWindow);
+  } else {
+    widget->show();
+  }
 
-    return app.exec();
+  return app.exec();
 }

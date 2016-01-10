4413f6119dbf95120f848c1e468eeb154a1db897
Cornelius Schumacher <schumacher@kde.org>
2016-01-10 18:45:09 +0100

    Implement yes_ship_it assertion for web page

--- yes_ship_it/assertions/web_site.rb
+++ yes_ship_it/assertions/web_site.rb
@@ -5,9 +5,19 @@ module YSI
     end
 
     def check
+      html = RestClient.get("http://cornelius-schumacher.de/polka")
+      if html =~ /Polka #{engine.version}/
+        engine.version
+      else
+        nil
+      end
     end
 
     def assert(executor)
+      engine.out.puts "..."
+      engine.out.puts "  Can't update the web page automatically."
+      engine.out.puts "  Please update the web page with version #{engine.version}"
+      "... #{engine.version}"
     end
   end
 end
